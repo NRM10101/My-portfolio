87caab38dd1f5ca683b7e9ba3b5fc99ce6a39ab8
NRM10101 <niman.ransindu@gmail.com>
2023-11-29 21:57:49 +0530

    added rotating functionalities

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import {Suspense} from 'react'
 import Loader from '../components/Loader'
 import Island from '../models/Island'
 import Sky from '../models/sky'
+import Plane from '../models/Plane'
+import Bird from '../models/Bird'
 
 const Home = () => {
+    const [isRotating,setIsRotating] = useState(false) 
+
     const adjustIslandForScreenSize =() => {
         let screenScale=null
         let screenPosition=[0,-6.5,-43];
@@ -19,13 +23,27 @@ const Home = () => {
 
         return [screenScale,screenPosition,rotation];
     }
-    const [islandScale,islandPosition,islandRotation]=adjustIslandForScreenSize();
+    const adjustPlaneForScreenSize =() => {
+        let screenScale,screenPosition;
+        // let rotation=[0.1,4.7,0];
 
+        if(window.innerHeight<768){ //for smaller screens
+            screenScale=[1.5,1.5,1.5];
+            screenPosition=[0,-1.5,0]
+        }else{
+            screenScale=[3,3,3];
+            screenPosition=[0.-4,-4]
+        }
+
+        return [screenScale,screenPosition];
+    }
+    const [islandScale,islandPosition,islandRotation]=adjustIslandForScreenSize();
+    const [planeScale,planePosition]=adjustPlaneForScreenSize();
     return (
         <section className='w-full h-screen relative'>
             {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>POPUP</div> */}
             <Canvas 
-                className='w-full h-screen relative'
+                className={`w-full h-screen bg-transparent ${isRotating ?'cursor-grabbing' : 'cursor-grab'}`}
                 camera={{near:0.1,far:1000}}
             >
                 <Suspense fallback={<Loader/>}>
@@ -33,11 +51,20 @@ const Home = () => {
                     <ambientLight intensity={0.5}/>
                     {/* <spotLight/> */}
                     <hemisphereLight skyColor='#b1e1ff' groundColor='#000000' intensity={1}/>
+                    <Bird/>
                     <Sky/>
                     <Island
                         position={islandPosition}
                         scale={islandScale}
                         rotation={islandRotation}
+                        isRotating={isRotating}
+                        setIsRotating={setIsRotating}
+                    />
+                    <Plane
+                        isRotating={isRotating}
+                        planeScale={planeScale}
+                        planePosition={planePosition}
+                        rotation={[0,20,0]}
                     />
                 </Suspense>
 
