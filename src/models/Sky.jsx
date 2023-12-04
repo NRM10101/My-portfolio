08d6cf1e4408da018aede96227f51fdedaa402a1
NRM10101 <niman.ransindu@gmail.com>
2023-12-04 23:32:39 +0530

    Rotating sky

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { useGLTF } from '@react-three/drei'
+import { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
 
 import skyScene from '../../Assets/assets/3d/sky.glb'
-const Sky = () => {
+const Sky = ({ isRotating }) => {
     const sky = useGLTF(skyScene)
+    const skyRef=useRef();
+
+    useFrame((_,delta)=>{
+        if(isRotating){
+            skyRef.current.rotation.y+=0.25*delta
+        }
+    })
+
     return (
-        <mesh>
-            <primitive object={sky.scene}/>
+        <mesh ref={skyRef}>
+            <primitive object={sky.scene} />
         </mesh>
     )
 }
